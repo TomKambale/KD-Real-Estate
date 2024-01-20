721b2d47e184df1a01fd5b3f7fecc9df9c3ba952
Thomas <thomaskambale1@gmail.com>
2024-01-20 15:09:53 +0300

    navbar changes 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css'; // Update the import statement to match the CSS file location
+import '../Navbar.css'; // Update the import statement to match the CSS file location
 
 function Navbar() {
     const handleContactClick = () => {
@@ -8,9 +8,9 @@ function Navbar() {
   return (
     <section id='nav' className='navigation'>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <h4 className='logo-text'>KD</h4>
 
         <div className="container-fluid">
-          <h4 className='logo-text'>KD</h4>
           <a className="navbar-brand text-bold" href="/">KD REAL ESTATE</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -22,10 +22,10 @@ function Navbar() {
             <a className="nav-link active" aria-current="page" href="#about">About</a>
             <a className="nav-link active" href="#services">Services</a>
             <a className="nav-link active" href="#testimonies">Testimonies</a>
-            <a className="nav-link active "></a>
-            <button className="contact-b" type='button' onClick={handleContactClick}>Contact Us</button>
-
-          </div>
+          </div>               
+          <a className="nav-link active "></a>
+         
+          <button className="contact-b" onClick={handleContactClick}>Contact</button>
         </div>
       </nav>
     </section>
